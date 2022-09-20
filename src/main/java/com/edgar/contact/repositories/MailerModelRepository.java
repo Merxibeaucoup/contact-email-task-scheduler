8553f158da979cb7fb0e225a90a4bf22118edb2b
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-09-19 22:01:31 -0400

    second

--- src/main/java/com/edgar/contact/repositories/MailerModelRepository.java
+++ src/main/java/com/edgar/contact/repositories/MailerModelRepository.java
@@ -1,13 +1,11 @@
 package com.edgar.contact.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.edgar.contact.models.MailerModel;
 
 @Repository
-public interface MailerModelRepository extends JpaRepository<MailerModel, UUID> {
+public interface MailerModelRepository extends JpaRepository<MailerModel, Long> {
 
 }
